83bd7a2fd7640bcd8d9ac10e1b745d5585597f6e
vengodelsur <ekatlmsh@yandex.ru>
2018-03-07 18:53:15 +0300

    add vslutov tests; awful setting through deleting entry with existing key; maxtest not passed

--- src/storage/MapBasedGlobalLockImpl.cpp
+++ src/storage/MapBasedGlobalLockImpl.cpp
@@ -71,12 +71,53 @@ bool MapBasedGlobalLockImpl::PutIfAbsent(const std::string &key, const std::stri
 // See MapBasedGlobalLockImpl.h
 bool MapBasedGlobalLockImpl::Set(const std::string &key, const std::string &value) { 
     std::unique_lock<std::mutex> lock(_mutex); //shared?
-    return false; 
+
+    auto iterator = _backend.find(key);
+
+    if (iterator == _backend.end()) {
+        return false;
+    } else {
+        //move existing key to tail and delete
+        _cache.splice(_cache.end(), _cache, iterator->second);
+        _cache.pop_back();
+    }
+
+    size_t entry_size = key.size() + value.size();
+
+    if (entry_size > _max_size) {
+        return false;
+    }
+
+    while (entry_size + _current_size > _max_size) {
+        auto tail = _cache.back();
+        size_t tail_size = tail.first.size() + tail.second.size();
+        _backend.erase(tail.first);
+        _cache.pop_back();
+        _current_size -= tail_size;
+    }
+
+    auto entry = std::make_pair(key, value);
+    _cache.emplace_front(entry);
+    _backend.emplace(key, _cache.cbegin());
+
+    return true; 
+    
 }
 
 // See MapBasedGlobalLockImpl.h
 bool MapBasedGlobalLockImpl::Delete(const std::string &key) { 
     std::unique_lock<std::mutex> lock(_mutex);
+
+    auto iterator = _backend.find(key);
+
+    if (iterator == _backend.end()) {
+        return true;
+    } else {
+        //move existing key to tail and delete
+        _cache.splice(_cache.end(), _cache, iterator->second);
+        _cache.pop_back();
+        return true;
+    }
     return false; 
 }
 
