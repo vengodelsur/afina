579d842910812c3cb7fc558b2ab1146f625fa271
vengodelsur <ekatlmsh@yandex.ru>
2018-03-08 23:33:06 +0300

    hw1 passing tests

--- src/storage/MapBasedGlobalLockImpl.cpp
+++ src/storage/MapBasedGlobalLockImpl.cpp
@@ -8,22 +8,24 @@ namespace Backend {
 // See MapBasedGlobalLockImpl.h
 bool MapBasedGlobalLockImpl::Put(const std::string &key, const std::string &value) { 
     std::unique_lock<std::mutex> lock(_mutex);   
-
-    auto iterator = _backend.find(key);
-
-    if (iterator != _backend.end()) {
-        //move existing key to tail and delete
-        _cache.splice(_cache.end(), _cache, iterator->second);
-        _cache.pop_back();
-        
-    } 
-
+    
     size_t entry_size = key.size() + value.size();
 
     if (entry_size > _max_size) {
         return false;
     }
 
+
+    auto iterator = _backend.find(key);
+
+    if (iterator != _backend.end()) {
+        //move existing key to tail and delete
+        _cache.splice(--_cache.end(), _cache, iterator->second);
+        _cache.pop_back();
+        
+    } 
+
+   
     while (entry_size + _current_size > _max_size) {
         auto tail = _cache.back();
         size_t tail_size = tail.first.size() + tail.second.size();
@@ -32,9 +34,8 @@ bool MapBasedGlobalLockImpl::Put(const std::string &key, const std::string &valu
         _current_size -= tail_size;
     }
 
-    auto entry = std::make_pair(key, value);
-    _cache.emplace_front(entry);
-    _backend.emplace(key, _cache.cbegin());
+    _cache.emplace_front(key, value);
+    _backend.emplace(_cache.front().first, _cache.cbegin());
     _current_size += entry_size;
 
     return true; 
