2fa8683c94f97fa15c5647e31fd9bab7c3070325
vengodelsur <ekatlmsh@yandex.ru>
2018-06-28 00:58:13 +0300

    restore explained

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -9,13 +9,14 @@
 namespace Afina {
 namespace Coroutine {
 
+// current coroutine uses context to save stack
 void Engine::Store(context &ctx) {
     char stack_cursor;
     ctx.Low = &stack_cursor;  // Low is coroutine stack start address as stored
                               // in context
     ctx.Hight = StackBottom;  // Hight is coroutine stack end address as stored
                               // in context, StackBottom is the same for Engine
-                              // member
+                              // member (it's where functions turn cooperative)
     uint32_t stack_size = ctx.Hight - ctx.Low;
     if (stack_size > std::get<1>(ctx.Stack)) {
         ctx.ExtendStack(stack_size);  // See include/afina/coroutine/Engine.h
@@ -24,6 +25,7 @@ void Engine::Store(context &ctx) {
            stack_size);  // Stack is stored as tuple of start address and size
 }
 
+// get stack by context and grant control to coroutine
 void Engine::Restore(context &ctx) {
     char stack_cursor;
     if (&stack_cursor >= ctx.Low) {
@@ -34,7 +36,8 @@ void Engine::Restore(context &ctx) {
     uint32_t stack_size = std::get<1>(ctx.Stack);
 
     memcpy(ctx.Low, stack_buffer, stack_size);
-    longjmp(ctx.Environment, 1);
+    longjmp(ctx.Environment, 1);  // loads context saved by latest setjmp;
+                                  // status 1 will be returned by setjmp
 }
 
 void Engine::yield() {}
