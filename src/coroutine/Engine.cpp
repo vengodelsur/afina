8f5d5bdeca54506a388fe1750f93620d84b4ae28
vengodelsur <ekatlmsh@yandex.ru>
2018-06-28 00:40:52 +0300

    restore

--- src/coroutine/Engine.cpp
+++ src/coroutine/Engine.cpp
@@ -4,31 +4,42 @@
 #include <stdio.h>
 #include <string.h>
 
-//Assumption: stack grows down (praises to x86)
+// Assumption: stack grows down (praises to x86)
 
 namespace Afina {
 namespace Coroutine {
 
 void Engine::Store(context &ctx) {
-
-        char stack_cursor;
-        ctx.Low = &stack_cursor; //Low is coroutine stack start address as stored in context
-        ctx.Hight = StackBottom; //Hight is coroutine stack end address as stored in context, StackBottom is the same for Engine member
-        uint32_t stack_size = ctx.Hight - ctx.Low;
-        if (stack_size > std::get<1>(ctx.Stack)) {
-            ctx.ExtendStack(stack_size); //See include/afina/coroutine/Engine.h
-        }
-        memcpy(std::get<0>(ctx.Stack), ctx.Low, stack_size); //Stack is stored as tuple of start address and size
-        
-
+    char stack_cursor;
+    ctx.Low = &stack_cursor;  // Low is coroutine stack start address as stored
+                              // in context
+    ctx.Hight = StackBottom;  // Hight is coroutine stack end address as stored
+                              // in context, StackBottom is the same for Engine
+                              // member
+    uint32_t stack_size = ctx.Hight - ctx.Low;
+    if (stack_size > std::get<1>(ctx.Stack)) {
+        ctx.ExtendStack(stack_size);  // See include/afina/coroutine/Engine.h
+    }
+    memcpy(std::get<0>(ctx.Stack), ctx.Low,
+           stack_size);  // Stack is stored as tuple of start address and size
 }
 
-void Engine::Restore(context &ctx) {}
+void Engine::Restore(context &ctx) {
+    char stack_cursor;
+    if (&stack_cursor >= ctx.Low) {
+        Restore(ctx);
+    }
+
+    char *stack_buffer = std::get<0>(ctx.Stack);
+    uint32_t stack_size = std::get<1>(ctx.Stack);
+
+    memcpy(ctx.Low, stack_buffer, stack_size);
+    longjmp(ctx.Environment, 1);
+}
 
 void Engine::yield() {}
 
 void Engine::sched(void *routine_) {}
 
-
-} // namespace Coroutine
-} // namespace Afina
+}  // namespace Coroutine
+}  // namespace Afina
