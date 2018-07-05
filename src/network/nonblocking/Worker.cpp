d7ce0bb17ab4b40fa088f8842f3896b8d775d6d3
vengodelsur <ekatlmsh@yandex.ru>
2018-07-05 15:18:37 +0300

    format

--- src/network/nonblocking/Worker.cpp
+++ src/network/nonblocking/Worker.cpp
@@ -1,24 +1,24 @@
 #include "Worker.h"
 
 #include <memory>
-#include <string>
 #include <stdexcept>
+#include <string>
 
-#include <iostream>
-#include <signal.h>
-#include <sys/stat.h>
-#include <sys/epoll.h>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <sys/signalfd.h>
 #include <errno.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <memory>
+#include <signal.h>
+#include <sys/epoll.h>
+#include <sys/signalfd.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <cstring>
+#include <iostream>
+#include <memory>
 
-#include "../../protocol/Parser.h"
 #include <afina/execute/Command.h>
+#include "../../protocol/Parser.h"
 #include "Utils.h"
 
 namespace Afina {
@@ -26,12 +26,10 @@ namespace Network {
 namespace NonBlocking {
 
 // See Worker.h
-Worker::Worker(std::shared_ptr<Afina::Storage> ps): _storage_ptr(ps) {}
+Worker::Worker(std::shared_ptr<Afina::Storage> ps) : _storage_ptr(ps) {}
 
 // See Worker.h
 Worker::~Worker() {}
-
-
 
 // See Worker.h
 void Worker::Start(int server_socket) {
@@ -39,7 +37,7 @@ void Worker::Start(int server_socket) {
 
     _running.store(true);
     _server_socket = server_socket;
-    
+
     // the same way as RunAcceptor in ServerImpl for blocking server
     if (pthread_create(&_thread, nullptr, Worker::RunWorkerProxy,
                        new WorkerInfo(this, _server_socket)) < 0) {
@@ -66,10 +64,9 @@ void Worker::Join() {
 }
 
 void *Worker::RunWorkerProxy(void *p) {
-
     std::cout << "network debug: " << __PRETTY_FUNCTION__ << std::endl;
-    WorkerInfo* parameters = reinterpret_cast<WorkerInfo*>(p);
-    Worker* worker = parameters->worker;
+    WorkerInfo *parameters = reinterpret_cast<WorkerInfo *>(p);
+    Worker *worker = parameters->worker;
     int _server_socket = parameters->server_socket;
     worker->OnRun(_server_socket);
     return 0;
@@ -84,22 +81,23 @@ void Worker::OnRun(int server_socket) {
     // 1. Create epoll_context here
 
     _server_socket = server_socket;
-    
+
     _epoll_fd = epoll_create(_max_events);
     if (_epoll_fd < 0) {
         throw std::runtime_error("Can't create epoll context");
     }
 
     // 2. Add server_socket to context
-    
-    Connection* server_connection = new Connection(_server_socket, _running, _storage_ptr); 
+
+    Connection *server_connection =
+        new Connection(_server_socket, _running, _storage_ptr);
 
     struct epoll_event event;
     event.events = EPOLLEXCLUSIVE | EPOLLHUP | EPOLLERR |
                    EPOLLIN;  // epoll_wait always waits for EPOLERR and
                              // EPOLLHUP, so it's not obligatory to set them in
                              // events
-    event.data.ptr = server_connection; // Philipp doesn't approve using map
+    event.data.ptr = server_connection;  // Philipp doesn't approve using map
     // for storing connections by socket, using event field for user data is
     // recommended
     struct epoll_event events_chunk[_max_events];
@@ -108,11 +106,13 @@ void Worker::OnRun(int server_socket) {
         throw std::runtime_error("Can't add server socket to context");
     }
 
-    // 3. Accept new connections, don't forget to call make_socket_nonblocking on the client socket descriptor
+    // 3. Accept new connections, don't forget to call make_socket_nonblocking
+    // on the client socket descriptor
 
     while (_running.load()) {
         // timeout -1 makes epoll_wait wait indefinitely
-        int events_number = epoll_wait(_epoll_fd, events_chunk, _max_events, -1);
+        int events_number =
+            epoll_wait(_epoll_fd, events_chunk, _max_events, -1);
         // returns the number of fds ready for I/O, 0 if no fd became ready
         // during the timeout, -1 for error (errno set).
         /*events is memory where we store elements of type
@@ -127,7 +127,7 @@ void Worker::OnRun(int server_socket) {
             // to events memory
         }
 
-    for (int i = 0; i < events_number; i++) {
+        for (int i = 0; i < events_number; i++) {
             Connection *connection =
                 reinterpret_cast<Connection *>(events_chunk[i].data.ptr);
             if (connection->socket == _server_socket) {
@@ -149,19 +149,28 @@ void Worker::OnRun(int server_socket) {
                         }
                     }
                 } else {
-    // 4. Add connections to the local context
-                    make_socket_non_blocking(client_socket); // uses fcntl function for managing file descriptor (sets O_NONBLOCK flag)
-                    _connections.emplace_back(std::move(new Connection(client_socket, _running, _storage_ptr)));
+                    // 4. Add connections to the local context
+                    make_socket_non_blocking(
+                        client_socket);  // uses fcntl function for managing
+                                         // file descriptor (sets O_NONBLOCK
+                                         // flag)
+                    _connections.emplace_back(std::move(
+                        new Connection(client_socket, _running, _storage_ptr)));
 
-                    event.data.ptr = _connections.back().get(); // get is unique_ptr member function returning pointer
-                    event.events = EPOLLHUP | EPOLLERR | EPOLLIN | EPOLLOUT ;
-                    
-                    if (epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, client_socket, &event) == -1) {
-                        throw std::runtime_error("Can't add connection to epoll context");
+                    event.data.ptr =
+                        _connections.back().get();  // get is unique_ptr member
+                                                    // function returning
+                                                    // pointer
+                    event.events = EPOLLHUP | EPOLLERR | EPOLLIN | EPOLLOUT;
+
+                    if (epoll_ctl(_epoll_fd, EPOLL_CTL_ADD, client_socket,
+                                  &event) == -1) {
+                        throw std::runtime_error(
+                            "Can't add connection to epoll context");
                     }
                 }
             } else {
-    // 5. Process connection events                
+                // 5. Process connection events
                 int client_socket = connection->socket;
                 if (events_chunk[i].events & EPOLLHUP ||
                     events_chunk[i].events & EPOLLERR) {
@@ -169,26 +178,28 @@ void Worker::OnRun(int server_socket) {
                     // close of the socket), EPOLERR stands for error condition
                     epoll_ctl(_epoll_fd, EPOLL_CTL_DEL, client_socket, NULL);
                     FinishWorkWithClient(client_socket);
-                } else if (events_chunk[i].events & (EPOLLIN | EPOLLOUT)) { // file is avaliable for read/write operations
+                } else if (events_chunk[i].events &
+                           (EPOLLIN | EPOLLOUT)) {  // file is avaliable for
+                                                    // read/write operations
                     if (!connection->Process(events_chunk[i].events)) {
-                        epoll_ctl(_epoll_fd, EPOLL_CTL_DEL, client_socket, NULL);
+                        epoll_ctl(_epoll_fd, EPOLL_CTL_DEL, client_socket,
+                                  NULL);
                         FinishWorkWithClient(client_socket);
                     }
                 } else {
-                    
                     FinishWorkWithClient(client_socket);
-                    throw std::runtime_error("Event type doesn't fit any of expected ones");
+                    throw std::runtime_error(
+                        "Event type doesn't fit any of expected ones");
                 }
             }
         }
     }
 
-
     CleanUp();
 }
 
 void Worker::CleanUp() {
-    for (auto &conn: _connections) {
+    for (auto &conn : _connections) {
         epoll_ctl(_epoll_fd, EPOLL_CTL_DEL, conn->socket, NULL);
     }
     _connections.clear();
@@ -197,7 +208,7 @@ void Worker::CleanUp() {
 void Worker::FinishWorkWithClient(int client_socket) {
     std::cout << "network debug: " << __PRETTY_FUNCTION__ << std::endl;
     // should we close client socket here?
-    //epoll_ctl(_epoll_fd, EPOLL_CTL_DEL, client_socket, NULL);
+    // epoll_ctl(_epoll_fd, EPOLL_CTL_DEL, client_socket, NULL);
     for (auto iter = _connections.begin(); iter != _connections.end(); iter++) {
         if ((*iter)->socket == client_socket) {
             _connections.erase(iter);
@@ -206,7 +217,6 @@ void Worker::FinishWorkWithClient(int client_socket) {
     }
 }
 
-
-} // namespace NonBlocking
-} // namespace Network
-} // namespace Afina
+}  // namespace NonBlocking
+}  // namespace Network
+}  // namespace Afina
--- src/network/nonblocking/Worker.h
+++ src/network/nonblocking/Worker.h
@@ -1,19 +1,19 @@
 #ifndef AFINA_NETWORK_NONBLOCKING_WORKER_H
 #define AFINA_NETWORK_NONBLOCKING_WORKER_H
 
-#include <memory>
-#include <atomic>
-#include <pthread.h>
-#include <vector>
-#include <string>
-#include <unistd.h>
 #include <afina/execute/Command.h>
-#include "../../protocol/Parser.h"
-#include <cstring>
+#include <pthread.h>
 #include <sys/epoll.h>
 #include <sys/socket.h>
 #include <sys/types.h>
+#include <unistd.h>
+#include <atomic>
+#include <cstring>
 #include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include "../../protocol/Parser.h"
 
 namespace Afina {
 
@@ -21,30 +21,28 @@ class Storage;
 
 namespace Network {
 namespace NonBlocking {
-const uint32_t EPOLLEXCLUSIVE = (1 << 28); // why do we have to set it explicitly?
+const uint32_t EPOLLEXCLUSIVE =
+    (1 << 28);  // why do we have to set it explicitly?
 
-enum class State {
-    ReadCommand,
-    ExtractArguments,
-    SendAnswer
-};
+enum class State { ReadCommand, ExtractArguments, SendAnswer };
 
 class Connection {
-public:
-    Connection(int _socket, std::atomic<bool>& running, std::shared_ptr<Afina::Storage> ps) : socket(_socket), state(State::ReadCommand), storage_ptr(ps), running(running) {
-    }
-    ~Connection(void) {
-        close(socket);
-    }
+   public:
+    Connection(int _socket, std::atomic<bool>& running,
+               std::shared_ptr<Afina::Storage> ps)
+        : socket(_socket),
+          state(State::ReadCommand),
+          storage_ptr(ps),
+          running(running) {}
+    ~Connection(void) { close(socket); }
 
-    
-    int socket;    
+    int socket;
     std::shared_ptr<Afina::Storage> storage_ptr;
     std::atomic<bool>& running;
 
     uint32_t command_body_size;
     std::unique_ptr<Execute::Command> resulting_command;
-    
+
     bool done = false;
     bool result;
 
@@ -65,130 +63,133 @@ public:
 
     bool ReadStep(char* start, size_t size) {
         read_length = recv(socket, start, size, 0);
-                       if (read_length <= 0) {
-                           if ((errno == EWOULDBLOCK || errno == EAGAIN) && read_length < 0 && running.load()) {
-                               result = true;
-                           } else {
-                               result = false;
-                           }
-                           return false;
-                       }    
+        if (read_length <= 0) {
+            if ((errno == EWOULDBLOCK || errno == EAGAIN) && read_length < 0 &&
+                running.load()) {
+                result = true;
+            } else {
+                result = false;
+            }
+            return false;
+        }
         return true;
     }
     bool ReadCommandStep() {
         return ReadStep(chunk + read_counter, CHUNK_SIZE - read_counter);
-        
     }
-    bool ExtractArgumentsStep() {
-         return ReadStep(chunk, CHUNK_SIZE);
-    }
+    bool ExtractArgumentsStep() { return ReadStep(chunk, CHUNK_SIZE); }
     bool SendAnswerStep() {
-        sent_length = send(socket, answer.data() + sent_counter, answer.size() - sent_counter, 0);
-                           if (sent_length < 0) {
-                                if ((errno == EWOULDBLOCK || errno == EAGAIN) && running.load()) {
-                                    result = true;
-                                } else {
-                                    result = false;
-                                }
-                                return false;
-                            }
+        sent_length = send(socket, answer.data() + sent_counter,
+                           answer.size() - sent_counter, 0);
+        if (sent_length < 0) {
+            if ((errno == EWOULDBLOCK || errno == EAGAIN) && running.load()) {
+                result = true;
+            } else {
+                result = false;
+            }
+            return false;
+        }
         return true;
     }
     bool Process(uint32_t events) {
-
         while (running.load()) {
             try {
-               if (state == State::ReadCommand) {
-                   parsed_length = 0;
-                   while (!parser.Parse(chunk, read_counter, parsed_length)) {
-                       std::memmove(chunk, chunk + parsed_length, read_counter - parsed_length);
-                       read_counter -= parsed_length;
+                if (state == State::ReadCommand) {
+                    parsed_length = 0;
+                    while (!parser.Parse(chunk, read_counter, parsed_length)) {
+                        std::memmove(chunk, chunk + parsed_length,
+                                     read_counter - parsed_length);
+                        read_counter -= parsed_length;
 
-                       if(!ReadCommandStep()) return result;
+                        if (!ReadCommandStep()) return result;
 
-                       read_counter += read_length;
-                   }
-                   std::memmove(chunk, chunk + parsed_length, read_counter - parsed_length);
-                   read_counter -= parsed_length;
+                        read_counter += read_length;
+                    }
+                    std::memmove(chunk, chunk + parsed_length,
+                                 read_counter - parsed_length);
+                    read_counter -= parsed_length;
 
-                   resulting_command = parser.Build(command_body_size);
-                   command_body_size += 2;
-                   parser.Reset();
+                    resulting_command = parser.Build(command_body_size);
+                    command_body_size += 2;
+                    parser.Reset();
 
-                   command_body.clear();
-                   state = State::ExtractArguments;
-               }
-               
-               if (state == State::ExtractArguments) {
-                   if (command_body_size > 2) {
-                       while (command_body_size > read_counter) {
-                           command_body.append(chunk, read_counter);
-                           command_body_size -= read_counter;
-                           read_counter = 0;
+                    command_body.clear();
+                    state = State::ExtractArguments;
+                }
 
+                if (state == State::ExtractArguments) {
+                    if (command_body_size > 2) {
+                        while (command_body_size > read_counter) {
+                            command_body.append(chunk, read_counter);
+                            command_body_size -= read_counter;
+                            read_counter = 0;
 
-                           if (!ExtractArgumentsStep()) return result;
+                            if (!ExtractArgumentsStep()) return result;
 
-                           read_counter = read_length;
-                       }
+                            read_counter = read_length;
+                        }
 
-                       command_body.append(chunk, command_body_size);
-                       std::memmove(chunk, chunk + command_body_size, read_counter - command_body_size);
-                       read_counter -= command_body_size;
+                        command_body.append(chunk, command_body_size);
+                        std::memmove(chunk, chunk + command_body_size,
+                                     read_counter - command_body_size);
+                        read_counter -= command_body_size;
 
-                       command_body = command_body.substr(0, command_body.length() - 2);
-                   }
+                        command_body =
+                            command_body.substr(0, command_body.length() - 2);
+                    }
 
-                   resulting_command->Execute(*storage_ptr, command_body, answer);
-                   answer.append("\r\n");
-                   state = State::SendAnswer;
-               }
+                    resulting_command->Execute(*storage_ptr, command_body,
+                                               answer);
+                    answer.append("\r\n");
+                    state = State::SendAnswer;
+                }
 
-           } catch (std::runtime_error &e) {
-               answer = std::string("SERVER_ERROR ") + e.what() + std::string("\r\n");
-               //std::cout << "error catched: " << e.what() <<  std::endl;
-               read_counter = 0;
-               parser.Reset();
-               state = State::SendAnswer;
-           }
+            } catch (std::runtime_error& e) {
+                answer = std::string("SERVER_ERROR ") + e.what() +
+                         std::string("\r\n");
+                // std::cout << "error catched: " << e.what() <<  std::endl;
+                read_counter = 0;
+                parser.Reset();
+                state = State::SendAnswer;
+            }
 
+            if (events & EPOLLOUT) {
+                if (state == State::SendAnswer) {
+                    if (answer.size() > 2) {
+                        while (sent_counter < answer.size()) {
+                            if (!SendAnswerStep()) return result;
 
-           if (events & EPOLLOUT) {
-               if (state == State::SendAnswer) {
-                   if (answer.size() > 2) {
-                       while (sent_counter < answer.size()) {
-                           
-                           if(!SendAnswerStep()) return result;
-
-                           sent_counter += sent_length;
-                       }
-                   }
-                   sent_counter = 0;
-                   
-               }
-           }
-           state = State::ReadCommand;
+                            sent_counter += sent_length;
+                        }
+                    }
+                    sent_counter = 0;
+                }
+            }
+            state = State::ReadCommand;
         }
         return false;
     }
 };
 class Worker {
-public:
+   public:
     Worker(std::shared_ptr<Afina::Storage> ps);
     ~Worker();
-    Worker(const Worker& w) : _storage_ptr(w._storage_ptr) {};
+    Worker(const Worker& w) : _storage_ptr(w._storage_ptr){};
 
     /**
      * Spaws new background thread that is doing epoll on the given server
-     * socket. Once connection accepted it must be registered and being processed
+     * socket. Once connection accepted it must be registered and being
+     * processed
      * on this thread
      */
     void Start(int server_socket);
 
     /**
      * Signal background thread to stop. After that signal thread must stop to
-     * accept new connections and must stop read new commands from existing. Once
-     * all readed commands are executed and results are send back to client, thread
+     * accept new connections and must stop read new commands from existing.
+     * Once
+     * all readed commands are executed and results are send back to client,
+     * thread
      * must stop
      */
     void Stop();
@@ -199,42 +200,39 @@ public:
      */
     void Join();
 
-
     pthread_t thread;
 
-protected:
+   protected:
     /**
      * Method executing by background thread
      */
     void OnRun(int server_socket);
 
-private:
+   private:
     struct WorkerInfo {
         Worker* worker;
         int server_socket;
 
-        WorkerInfo(Worker *p, int number)
-            : worker(p), server_socket(number) {}
-    };    
+        WorkerInfo(Worker* p, int number) : worker(p), server_socket(number) {}
+    };
 
-    static void *RunWorkerProxy(void *p);
+    static void* RunWorkerProxy(void* p);
     void FinishWorkWithClient(int client_socket);
     void CleanUp();
-    
+
     pthread_t _thread;
     int _server_socket;
     size_t _max_events = 32;
 
-    std::atomic<bool> _running; //enum class to tell between stopping and stopped worker?
+    std::atomic<bool>
+        _running;  // enum class to tell between stopping and stopped worker?
     std::vector<std::unique_ptr<Connection>> _connections;
-   
+
     int _epoll_fd;
     std::shared_ptr<Afina::Storage> _storage_ptr;
-
-
 };
 
-} // namespace NonBlocking
-} // namespace Network
-} // namespace Afina
-#endif // AFINA_NETWORK_NONBLOCKING_WORKER_H
+}  // namespace NonBlocking
+}  // namespace Network
+}  // namespace Afina
+#endif  // AFINA_NETWORK_NONBLOCKING_WORKER_H
